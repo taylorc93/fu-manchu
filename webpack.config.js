9e80132e573b5be0acd2da818bfb67a26c5c83a1
Connor Taylor <connor@cogolabs.com>
2017-06-29 16:28:31 -0400

    more work

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const BabiliPlugin = require("babili-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        enforce: 'pre',
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ],
+};
